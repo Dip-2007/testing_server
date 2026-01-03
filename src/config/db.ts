7abb5fe992eade922f1de869d8fbf7e1e4c6232f
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2026-01-03 20:24:12 +0530

    Changed to Vercel MongodDB

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import logger from './logger';
 
 const connectDB = async (): Promise<void> => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        const conn = await mongoose.connect(process.env.MONGODB_URI as string);
         logger.info(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         logger.error(`MongoDB Error: ${error}`);
